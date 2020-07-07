b3982b9b8944c0e66a99e8cab17417289ed26064
Smith <khushipan17@gmail.com>
2020-07-07 17:18:01 -0400

    all components are created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,23 @@ import { FormsModule } from '@angular/forms';
 
 
 import { AppComponent } from './app.component';
+import {headercomponent} from './header/header.component';
+import { RecipeComponent } from './recipe/recipe.component';
+import { RecipeDetailsComponent } from './recipe/recipe-details/recipe-details.component';
+import {RecipeItemComponent} from './recipe/recipe-details/recipe-item/recipe-item.component';
+import {ShoppingListComponent} from './shopping-list/shopping-list.component';
+import {ShoppingListEditComponent} from './shopping-list/shopping-list-edit/shopping-list-edit.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RecipeComponent,
+    headercomponent,
+    RecipeDetailsComponent,
+    ShoppingListComponent,
+    ShoppingListEditComponent
+    
   ],
   imports: [
     BrowserModule,
